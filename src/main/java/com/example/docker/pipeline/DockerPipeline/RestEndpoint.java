309f66a443ee0db5afdf3e62da66635a72872801
Pulkit Gupta <pulgupta@gmail.com>
2017-06-04 00:09:05 +0530

    added some comments

--- src/main/java/com/example/docker/pipeline/DockerPipeline/RestEndpoint.java
+++ src/main/java/com/example/docker/pipeline/DockerPipeline/RestEndpoint.java
@@ -3,6 +3,12 @@ package com.example.docker.pipeline.DockerPipeline;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Its just a sample endpoint to test that our application is working
+ * once it is deployed and running as a container.
+ * @author pulgupta
+ *
+ */
 @RestController
 public class RestEndpoint {
 	@RequestMapping("/test")
